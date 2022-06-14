4ce0139909275c002b1701f54c961f46502ba6ce
skostychev <serjkostychev@gmail.com>
2022-06-14 11:25:27 -0400

    Add files via upload

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
 from brain_even import is_even
+import prompt
 
-def greet(where):
-    print('Welcome to the {}!'.format(where))
+
+def greet():
+    print('Welcome to the Brain Games!')
+
 
 def main():
-    greet('Brain Games')
+    greet()
+
 
 if __name__ == '__main__':
     main()
