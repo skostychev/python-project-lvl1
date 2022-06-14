7ac39066f11ac2542d435ecfb6ee86a1082b05cd
skostychev <serjkostychev@gmail.com>
2022-06-14 11:24:57 -0400

    Add files via upload

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
+import prompt
 
-def welcome_user(name):
+def welcome_user(game_name):
+  name = prompt.string("May I have your name?")
+  print(str("Hello {name}!\n{game_name}"))
+  return name
+
+
   
-  while name == ' ':
-    print('May I have your name? ', end='')
-    name = input()
-    print(f'Hello, {name}!')
+#   while name == ' ':
+#     print('May I have your name? ', end='')
+#     name = input()
+#     print(f'Hello, {name}!')
 
-welcome_user(name = ' ')
+# welcome_user(name = ' ')
